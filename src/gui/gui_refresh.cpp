aa42128d4e30e2c27fbfc21943e5a7377c1c8893
NA <nomec4842@gmail.com>
2025-11-29 16:57:12 -0500

    Move view menu updates into gui_refresh

--- src/gui/gui_refresh.cpp
+++ src/gui/gui_refresh.cpp
@@ -1,4 +1,7 @@
 #include "gui/gui_refresh.h"
+#include "gui/menu_commands.h"
+#include "gui/mod_matrix_window.h"
+#include "gui/waveform_window.h"
 
 #ifndef WIN32_LEAN_AND_MEAN
 #define WIN32_LEAN_AND_MEAN
@@ -10,7 +13,26 @@
 
 extern HWND gMainWindow;
 extern HMENU gViewMenu;
-void updateViewMenuChecks();
+extern HWND gPianoRollWindow;
+extern HWND gEffectsWindow;
+
+void updateViewMenuChecks()
+{
+    if (!gViewMenu)
+        return;
+
+    UINT pianoState = (gPianoRollWindow && IsWindow(gPianoRollWindow)) ? MF_CHECKED : MF_UNCHECKED;
+    CheckMenuItem(gViewMenu, kMenuCommandTogglePianoRoll, MF_BYCOMMAND | pianoState);
+
+    UINT effectsState = (gEffectsWindow && IsWindow(gEffectsWindow)) ? MF_CHECKED : MF_UNCHECKED;
+    CheckMenuItem(gViewMenu, kMenuCommandToggleEffects, MF_BYCOMMAND | effectsState);
+
+    UINT waveformState = isWaveformWindowOpen() ? MF_CHECKED : MF_UNCHECKED;
+    CheckMenuItem(gViewMenu, kMenuCommandToggleWaveform, MF_BYCOMMAND | waveformState);
+
+    UINT modMatrixState = isModMatrixWindowOpen() ? MF_CHECKED : MF_UNCHECKED;
+    CheckMenuItem(gViewMenu, kMenuCommandToggleModMatrix, MF_BYCOMMAND | modMatrixState);
+}
 
 void requestMainMenuRefresh()
 {
